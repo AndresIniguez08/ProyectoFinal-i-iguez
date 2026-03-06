aa95e305bae15c06620f1f1474b68dc6d2acebd2
Andy <andres@granjaalmeyra.com.ar>
2026-03-06 18:55:51 -0300

    mejor el boton detalles y agrego un pequeño hover

--- Primera entrega/src/components/Item.jsx
+++ Primera entrega/src/components/Item.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 const Item = ({ item }) => {
   return (
-    <article className="border rounded-lg p-3 bg-white shadow-sm">
+    <article className="border rounded-lg p-3 bg-white shadow-sm transition hover:shadow-md hover:-translate-y-1">
+
       <img
         src={item.pictureUrl}
         alt={item.title}
@@ -11,9 +12,14 @@ const Item = ({ item }) => {
       <h3 className="font-semibold">{item.title}</h3>
       <p className="text-sm text-gray-600">${item.price}</p>
 
-      <Link to={`/item/${item.id}`} className="text-sm text-blue-600 underline">
-        Ver detalle
-      </Link>
+      <Link
+  to={`/item/${item.id}`}
+  className="mt-3 block w-full rounded-md bg-gray-900 py-2 text-center text-sm font-medium text-white transition hover:bg-gray-800"
+>
+  Ver detalle
+</Link>
+
+
     </article>
   );
 };
